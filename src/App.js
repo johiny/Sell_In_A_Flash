ae48fb3828079e70ddcb89703762fa0176e33f99
omar rodriguez <omardevanisrodriguez@gmail.com>
2021-10-03 20:24:05 -0500

    pasar el codigo html y css a react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './imagenes/logo.png';
+import './estilos/estilos.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+        <img src={logo} alt="logo" className="logo"/>
+    </header>
+    <main>
+        <form className="formulario">
+            <div>
+                <h1>Sell In A Flash</h1>
+            </div>
+            <div className="posicion">
+                <div className="caja-input">
+                    <i className="fas fa-user icono"></i>
+                    <input type="text" placeholder="ID Usuario"/>
+                </div>
+                <div className="caja-input">
+                    <i className="fas fa-key icono"></i>
+                    <input type="password" placeholder="Contraseña"/>
+                </div>
+                <div>
+                    <button className="boton-generico boton-entrar">Entrar</button>
+                </div>
+                <div>
+                    <button className="boton-generico"><i className="fas fa-envelope"></i> Entrar Por Correo Electronico</button>
+                </div>
+                <div>
+                    <a href="">¿ Recuperar Contraseña ?</a>
+                </div>
+            </div>
+        </form>
+    </main>
+    <footer></footer>
     </div>
   );
 }
