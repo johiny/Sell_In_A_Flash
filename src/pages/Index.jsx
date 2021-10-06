eea08f8e46e1c65d181fffde230c8cce0a30d272
omar rodriguez <omardevanisrodriguez@gmail.com>
2021-10-06 17:28:27 -0500

    crear estructura de la pagina index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,31 @@
+import logo from "../imagenes/logo.png"
+
 function Index(){
     return(
-        <div>pagina index</div>
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Info Ventas</li>
+                        <li>Estado Ventas</li>
+                    </ul>    
+                </nav>
+                <div>
+                    <ul>
+                        <li>logo</li>
+                        <li>Gestión</li>
+                    </ul>
+                </div>
+            </header>
+            <main>
+                <ul>
+                    <li>Administra tus ventas de manera sencilla</li>
+                    <li>Comenzar</li>
+                </ul>
+            </main>
+            <footer></footer>
+        </div>
     );
 }
 
