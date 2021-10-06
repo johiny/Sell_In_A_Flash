f6ba62243e76406c51b24f42390722e761e2a92a
omar rodriguez <omardevanisrodriguez@gmail.com>
2021-10-06 14:42:02 -0500

    guardar cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Login from './pages/Login';
 import Index from './pages/Index'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './estilos/estilos.css';
 
 function App() {
