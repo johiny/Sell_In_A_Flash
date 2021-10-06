18abec700d52bfd6ab7abb20170cc6b703954649
omar rodriguez <omardevanisrodriguez@gmail.com>
2021-10-06 13:19:19 -0500

    agregar impot BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Login from './pages/Login'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './estilos/estilos.css';
 
 function App() {
   return (
     <div className="App">
-        <Login/>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
