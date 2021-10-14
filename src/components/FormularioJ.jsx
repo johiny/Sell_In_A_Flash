22c4d5614a088ba461a4d1dd7304915308fb70e2
Johiny <johiny7@live.com>
2021-10-14 12:43:59 -0500

    Estilos agregados al registro de ventas

--- src/components/FormularioJ.jsx
+++ src/components/FormularioJ.jsx
@@ -3,16 +3,15 @@ const FormularioJ = (props) => {
 
     return(
         <form className={props.estilo}>
-            <h1>ID</h1>
-            <label>{props.campo1}</label><input type="date" onChange={() => { props.setSaveChanges(true)
+            <label for={props.campo1}>{props.campo1}</label><input name={props.campo1} type="date" onChange={() => { props.setSaveChanges(true)
                         props.setShowmodal(true)}}></input>
-            <laber>{props.campo2}</laber><input type="number" onInput={() => { props.setSaveChanges(true)
+            <label for={props.campo2}>{props.campo2}</label><input name={props.campo2} type="number" onInput={() => { props.setSaveChanges(true)
                         props.setShowmodal(true)}}></input>
-            <laber>{props.campo3}</laber><input type="number" onInput={() => { props.setSaveChanges(true)
+            <label for={props.campo3}>{props.campo3}</label><input name={props.campo3} type="number" onInput={() => { props.setSaveChanges(true)
                         props.setShowmodal(true)}}></input>
-            <laber>{props.campo4}</laber><input type="text" onInput={() => { props.setSaveChanges(true)
+            <label for={props.campo4}>{props.campo4}</label><input name={props.campo4} type="text" onInput={() => { props.setSaveChanges(true)
                         props.setShowmodal(true)}}></input>
-            <laber>{props.campo5}</laber><input type="number" onInput={() => { props.setSaveChanges(true)
+            <label for={props.campo5}>{props.campo5}</label><input name={props.campo5} type="number" onInput={() => { props.setSaveChanges(true)
                         props.setShowmodal(true)}}></input>
             <select onChange={() => { props.setSaveChanges(true)
                         props.setShowmodal(true)}} name="estado" id="tabla__estado_selector" className="campo">
--- src/pages/Maestro-Ventas/NuevaVenta.jsx
+++ src/pages/Maestro-Ventas/NuevaVenta.jsx
@@ -9,7 +9,8 @@ const NuevaVenta = () => {
         <>
         <h1 className="cuerpo_central__Ventas-tabla-titulo">Nueva Venta</h1>
         <div className="tabla">
-        <FormularioJ campo1="Fecha" campo2="Cantidad" campo3="Cliente" campo4="Encargado" campo5="Valor Total" campo6="Estado"  setShowmodal={setShowmodal} setSaveChanges={setSaveChanges}/>
+        <h1 className="TituloNuevaVenta">ID</h1>
+        <FormularioJ estilo="FormularioNuevaVenta" campo1="Fecha Venta" campo2="Cantidad Venta" campo3="Documento Cliente" campo4="Nombre Encargado" campo5="Valor Total" campo6="Estado"  setShowmodal={setShowmodal} setSaveChanges={setSaveChanges}/>
         </div>
         {showmodal && 
         <ModalCambios setSaveChanges={setSaveChanges} savechanges={savechanges}/>
