1e2e609e569ee8ddf792c698a40328995b8ce81c
Johiny <johiny7@live.com>
2021-10-17 09:31:30 -0500

    cambio de guardado pop up, emergente al editar no funciona

--- src/components/VentaRow.jsx
+++ src/components/VentaRow.jsx
@@ -1,20 +1,51 @@
 import {Link} from "react-router-dom"
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import trashcan from "media/delete.png"
+import pencil from "media/pencil.svg"
+import cross from "media/cancel.svg"
 const VentaRow = (props) => {
+
+    const [editActivate,setEditActivate] = useState(false);
+
+    
+
+    const edittrigger = () => {
+
+        setEditActivate(!editActivate)
+        console.log(editActivate)
+    }
+
     return(
 <tr>
     <td><Link to={`/Maestro-Ventas/${props.venta.ID}`}>1</Link></td>
-    <td className="campo"><input defaultValue={props.venta.Fecha}></input></td>
-    <td className="campo"><input defaultValue={props.venta.Cantidad}></input></td>
-    <td className="campo"><input defaultValue={props.venta.Cliente}></input></td>
-    <td className="campo"><input defaultValue={props.venta.Encargado}></input></td>
-    <td className="campo"><input defaultValue={props.venta.Valor_Total}></input></td>
-    <td><select defaultValue ={props.venta.Estado} onChange={() => { props.setSaveChanges(true)
-                        props.setShowmodal(true)}} name="estado" id="tabla__estado_selector" className="campo">
+
+    {editActivate ? <td className="campo"><input name="fecha" defaultValue={props.venta.Fecha}></input></td> : 
+    <td className="campo">{props.venta.Fecha}</td>}
+
+    {editActivate ? <td className="campo"><input name="cantidad" defaultValue={props.venta.Cantidad}></input></td> : 
+    <td className="campo">{props.venta.Cantidad}</td>}
+
+    {editActivate ? <td className="campo"><input name="cliente" defaultValue={props.venta.Cliente}></input></td> :
+    <td className="campo">{props.venta.Cliente}</td>}
+
+    {editActivate ? <td className="campo"><input name="encargado" defaultValue={props.venta.Encargado}></input></td> :
+    <td className="campo">{props.venta.Encargado}</td>}
+
+    {editActivate ? <td className="campo"><input name="valor_total" defaultValue={props.venta.Valor_Total}></input></td> :
+    <td className="campo">{props.venta.Valor_Total}</td>}
+
+    {editActivate ? <td><select defaultValue={props.venta.Estado} name="estado" id="tabla__estado_selector" className="campo">
                         <option value="Proceso">En Proceso</option>
                         <option value="Cancelada">Cancelada</option>
                         <option value="Entregada">Entregada</option>
-    </select></td>
-    <td><img className="tabla__trashcan" src={props.ico}></img></td>
+    </select></td> :
+    <td>{props.venta.Estado}</td>}
+
+    <td><div>
+    <img className="tabla__trashcan" src={editActivate ? cross : pencil} onClick={ () => edittrigger()}></img>
+    <img className="tabla__trashcan" src={trashcan}></img>
+    </div></td>
     
 </tr>
     )
