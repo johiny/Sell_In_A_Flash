44e09341f3bfba43e605662abb38baf49ccc3d10
Johiny <johiny7@live.com>
2021-10-17 20:38:41 -0500

    porfin! buscador funcional!

--- src/pages/Maestro-Ventas/Table.jsx
+++ src/pages/Maestro-Ventas/Table.jsx
@@ -5,18 +5,28 @@ import ModalCambios from "components/ModalCambios";
 import ventas from "Ventas_test.json"
 import { ToastContainer } from "react-toastify";
 const TableIndex = () => {
-    const [listaVentas, setListaVentas] = useState([]);
+    const [listaVentas, setListaVentas] = useState(ventas);
+    const [listaFiltrada,setListaFiltrada] = useState(listaVentas);
+    const [busqueda,setBusqueda] = useState("");
+
+    useEffect(() => {
+        setListaFiltrada(
+        listaVentas.filter(elemento => {
+            return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+                })
+        )
+    },[busqueda])
     
     useEffect(() => {
-        setListaVentas(ventas)
-    },[])
+        console.log(listaVentas)
+    },[listaVentas])
 
     
     return(
         <>
         <h1 className="cuerpo_central__Ventas-tabla-titulo">Ventas</h1>
-        <Buscador estilo="Buscador_Tabla_Ventas"/>
-        <Tabla ventasbackend={listaVentas}/>
+        <Buscador busqueda={busqueda} setBusqueda={setBusqueda} estilo="Buscador_Tabla_Ventas"/>
+        <Tabla ventasbackend={listaFiltrada}/>
         <ToastContainer position="bottom-right"
             autoClose={5000}
             hideProgressBar={false}
