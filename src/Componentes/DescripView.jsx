0d90c585b592d0114a17ee219713eae1bfac3d6d
dianeagle <jliette92@hotmail.com>
2021-10-27 03:25:30 -0500

    Previo al merge

---
+++ src/Componentes/DescripView.jsx
@@ -0,0 +1,14 @@
+
+const DescripView = () => {
+    return(
+        <div>
+            <li>
+        <label for="ID">
+          <textarea className="Datos" name="ID" placeholder='Descripción'/>
+        </label>
+            </li>
+        </div>
+    )
+}
+
+export default DescripView
--- src/Pages/Ver.jsx
+++ src/Pages/Ver.jsx
@@ -1,9 +1,28 @@
+import '../Styles/View.css';
+import BotonesEdición from '../Componentes/BotonesEdición';
+import NavBarView from '../Componentes/NavBarView';
+import CampoAEditar from '../Componentes/CampoAEditar';
+import ListaDesplegable from '../Componentes/ListaEdición';
+import DescripView from '../Componentes/DescripView';
+import CampoID from '../Componentes/CampoID';
+import CamposAdd from '../Componentes/CamposAdd';
 
 function ViewRegister(){
     return(
         <div>
-            Ver contacto
-        </div>
+          <NavBarView/>
+          <CampoAEditar/>
+          <CampoAEditar/>
+          <ListaDesplegable/>
+          <DescripView/>
+          <CampoID/>
+          <CamposAdd/>
+          <CamposAdd/>
+          <CamposAdd/>
+          <BotonesEdición Nombre='Editar'/>
+          <BotonesEdición Nombre='Guardar'/>
+    </div>
+
     );
 }
 
