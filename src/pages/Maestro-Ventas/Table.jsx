d2207e29f45b48447442d42baf16a83e9b4a9dc4
Johiny <johiny7@live.com>
2023-01-26 17:38:23 -0500

    arreglo de conflictos

--- src/pages/Maestro-Ventas/Table.jsx
+++ src/pages/Maestro-Ventas/Table.jsx
@@ -1,11 +1,8 @@
 import Buscador from "components/Buscador";
 import Tabla from "components/Tabla";
 import { useEffect,useState } from "react";
-import ModalCambios from "components/ModalCambios";
-import ventas from "Ventas_test.json"
 import { obtenerVentas } from "utils/api";
 import { ToastContainer } from "react-toastify";
-import axios from "axios";
 const TableIndex = () => {
 
 const [listaVentas, setListaVentas] = useState([]);
